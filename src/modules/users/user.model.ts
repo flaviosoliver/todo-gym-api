430a003145806d0e014cdc3fe335acddda228aea
flaviosoliver <flavsoliver@gmail.com>
2023-05-17 19:39:54 -0300

    users model

---
+++ src/modules/users/user.model.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { ShapeHistory, ShapeHistorySchema } from './shape-history';
+
+export type UserDocument = User & Document;
+
+@Schema({ collection: 'users', timestamps: true })
+export class User {
+  id: string;
+
+  @Prop({ type: String, required: true })
+  email: string;
+
+  @Prop({ type: String, required: true })
+  password: string;
+
+  @Prop({ type: String, required: true })
+  firstName: string;
+
+  @Prop({ type: String, required: true })
+  lastName: string;
+
+  @Prop({ type: Date, required: true })
+  birth: Date;
+
+  @Prop({ type: [ShapeHistorySchema], required: true })
+  shape: ShapeHistory[];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
