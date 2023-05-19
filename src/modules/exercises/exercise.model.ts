71344e60671e6c14a2a957e6e195f31bc1877cfc
flaviosoliver <flavsoliver@gmail.com>
2023-05-19 16:35:26 -0300

    exercise model

---
+++ src/modules/exercises/exercise.model.ts
@@ -0,0 +1,29 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+export type ExerciseDocument = Exercise & Document;
+
+@Schema({ collection: 'exercises', timestamps: true })
+export class Exercise {
+  id: string;
+
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: mongoose.Schema.Types.String, required: true })
+  focusMustache: string[];
+
+  @Prop({ type: String, required: true })
+  image: string;
+
+  @Prop({ type: String, required: true })
+  video: string;
+
+  @Prop({ type: String, required: true })
+  load: string;
+
+  @Prop({ type: String, required: true })
+  notes: string;
+}
+
+export const ExerciseSchema = SchemaFactory.createForClass(Exercise);
