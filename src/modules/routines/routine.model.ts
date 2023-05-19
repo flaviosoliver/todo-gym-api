27e7e43e13ad62e9cbc0c5036d3dc358b74d8e4e
flaviosoliver <flavsoliver@gmail.com>
2023-05-19 18:40:44 -0300

    routine model

---
+++ src/modules/routines/routine.model.ts
@@ -0,0 +1,31 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { Training, TrainingSchema } from '../shared/training.model';
+
+export type RoutineDocument = Routine & Document;
+
+@Schema({ collection: 'routines', timestamps: true })
+export class Routine {
+  id: string;
+
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: mongoose.Schema.Types.String, required: true })
+  focusMuscle: string[];
+
+  @Prop({ type: String, required: true })
+  frequency: string;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Users',
+  })
+  userId: string;
+
+  @Prop({ type: [TrainingSchema], required: true })
+  training: Training[];
+}
+
+export const RoutineSchema = SchemaFactory.createForClass(Routine);
