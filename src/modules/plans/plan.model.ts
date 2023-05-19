e3e887d641f8362b337c1ab4e9da6966032925ed
flaviosoliver <flavsoliver@gmail.com>
2023-05-19 18:40:29 -0300

    plan model

---
+++ src/modules/plans/plan.model.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { Training, TrainingSchema } from '../shared/training.model';
+
+export type PlanDocument = Plan & Document;
+
+@Schema({ collection: 'plans', timestamps: true })
+export class Plan {
+  id: string;
+
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: mongoose.Schema.Types.String, required: true })
+  focusMuscle: string[];
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Users',
+  })
+  userId: string;
+
+  @Prop({ type: [TrainingSchema], required: true })
+  training: Training[];
+}
+
+export const PlanSchema = SchemaFactory.createForClass(Plan);
