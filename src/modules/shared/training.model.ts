ce833d325ce6a763d9b19574884c07cdeafe1eb2
flaviosoliver <flavsoliver@gmail.com>
2023-05-19 18:40:58 -0300

    training model

---
+++ src/modules/shared/training.model.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+
+export type TrainingDocument = Training & Document;
+
+@Schema({ timestamps: true })
+export class Training {
+  id: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, required: true })
+  exerciseId: string;
+
+  @Prop({ type: String, required: true })
+  series: string;
+
+  @Prop({ type: String, required: true })
+  repetitions: string;
+
+  @Prop({ type: String, required: true })
+  notes: string;
+}
+
+export const TrainingSchema = SchemaFactory.createForClass(Training);
