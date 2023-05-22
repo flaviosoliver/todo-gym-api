e260d21ab927dc159e35d6b21af418547a951c3f
flaviosoliver <flavsoliver@gmail.com>
2023-05-22 15:44:39 -0300

    config mongodb

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from '../app.controller';
-import { AppService } from '../app.service';
-import { PlansModule } from './plans/plans.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { RoutinesModule } from './routines/routines.module';
 import { ExercisesModule } from './exercises/exercises.module';
 import { UsersModule } from './users/users.module';
+import { PlansModule } from './plans/plans.module';
+import { AppController } from '../app.controller';
+import { AppService } from '../app.service';
 
 @Module({
-  imports: [PlansModule, RoutinesModule, ExercisesModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority`,
+      }),
+    }),
+    PlansModule,
+    RoutinesModule,
+    ExercisesModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
