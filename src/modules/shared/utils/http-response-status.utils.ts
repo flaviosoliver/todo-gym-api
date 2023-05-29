1a00c6c871db096005b28b0d46a8b6ab962bb12b
flaviosoliver <flavsoliver@gmail.com>
2023-05-29 12:54:19 -0300

    status http

---
+++ src/modules/shared/utils/http-response-status.utils.ts
@@ -0,0 +1,12 @@
+// Successful responses
+export const OK_200 = 'OK';
+export const CREATED_201 = 'CREATED';
+export const NO_CONTENT_204 = 'NO CONTENT';
+
+// Client error responses
+export const BAD_REQUEST_400 = 'BAD REQUEST';
+export const UNAUTHORIZED_401 = 'UNAUTHORIZED';
+export const NOT_FOUND_404 = 'NOT FOUND';
+
+// Server error responses
+export const SERVER_ERROR_500 = 'INTERNAL SERVER ERROR';
